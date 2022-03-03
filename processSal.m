5458ca7c09fa7e44b6df4fcaa6428cf970a160fb
Henk Jongbloed <henkjongbloed@live.com>
2022-03-03 19:36:09 +0100

    Quick cleanup
    To do: Look at rescale sigma coordinates since in the RWW the CTD does not reach bottom anymore.

--- processSal.m
+++ processSal.m
@@ -8,12 +8,14 @@ S.S          =  ctd.S; %salinity
 adcpT = datenum(V.raw.timeV ) ; % in days
 hour = datenum('12-Aug-2014 01:00:00') - datenum('12-Aug-2014 00:00:00');
 
-[adcpH, aindh] = getHour(adcpT, hour/2); % ADCP hours
+[adcpH, ~] = getHour(adcpT, hour/2); % ADCP hours
 % WGS2UTM ook bij ADCPData gebruikt?? ->
 if strcmp(DS,'NMOMNW15')    % Does not work correctly yet
     [S.X, S.Y]      = wgs2utm(ctd.Y, ctd.X); %Convert LatLon to UTM - pay close attention to coordinate systems. WGS84 - ETRS89 differs at most 65 cm, not relevant
-    [ctdH, cindh] = getHour(ctd.T2, hour/2);% + datenum('9-Aug-2014 00:00:00'); % Correct
+    % Time
+    [ctdH, ~] = getHour(ctd.T2, hour/2);% + datenum('9-Aug-2014 00:00:00'); % Correct
     S.t = rescale(ctdH, min(adcpH), max(adcpH)); % Everything works perfectly
+    % Space
     xnew = center_cloud([S.X, S.Y], U.xs.origin'); % Because of offset in CTD coordinates relative to ADCP - quick fix
     S.X = xnew(:,1); S.Y = xnew(:,2); % Only needed for 2015 data
 
@@ -21,7 +23,7 @@ elseif strcmp(DS,'OMHA14')
     [templon, templat, ~]      = rd2wgs(ctd.X,ctd.Y); %Convert RD to LatLon %Correct
     [S.X, S.Y]      = wgs2utm(templat, templon); %Convert LatLon to UTM %Correct
     ctddt = 6000;
-    [ctdH, cindh] = getHour(ctd.T, ctddt); % dt = 6000 -> 1 sample per 6s?
+    [ctdH, ~] = getHour(ctd.T, ctddt); % dt = 6000 -> 1 sample per 6s?
     S.t = rescale(ctdH, min(adcpH), max(adcpH));
     if strcmp(U.BN, ' OMN')
         S.t = manualOMN(ctd.T, adcpH);
@@ -33,47 +35,55 @@ elseif strcmp(DS,'OMHA14')
 %     S.t = ctd.T;% + datenum('12-Aug-2014 00:00:00');
     %U.mesh_mean.plot()
 end
+% disp(numel(unique(adcpH)))
+% disp(numel(unique(S.t)))
+% disp(max(adcpH) - max(S.t))
+% disp(min(adcpH) - min(S.t))
+% datestr(max(adcpH)- min(adcpH))
 
+% t2 = datenum(adcpT);
+% 
+% figure;
+% plot(adcpH)
+% hold on
+% plot(S.t)
+U.adcpH = adcpH;
 
-t2 = datenum(adcpT);
-
-figure;
-plot(adcpH)
-hold on
-plot(S.t)
-
-
-S.eta = U.eta(S.t);
-
-figure;
-subplot(211)
-plot(ctd.X, ctd.Y)
-
-subplot(212)
-try
-    plot(templon, templat)
-
-figure
-plot(S.X, S.Y, '*')
-hold on
-plot(xnew(:,1), xnew(:,2), '*')
-U.B.plot()
-U.xs.plot()
-catch
-    
-end
+S.eta = U.eta(S.t); % Waterlevel per CTD sample
+% plot(S.eta)
+% 
+% figure;
+% subplot(211)
+% plot(ctd.X, ctd.Y)
+% 
+% subplot(212)
+% try
+%     plot(templon, templat)
+% 
+% figure
+% plot(S.X, S.Y, '*')
+% hold on
+% plot(xnew(:,1), xnew(:,2), '*')
+% U.B.plot()
+% U.xs.plot()
+% catch
+%     
+% end
 
 S.Z          =  - ctd.Z + S.eta;
 
 
-temp = datetime(S.t, 'ConvertFrom','datenum','Format','HH:mm:ss');
 
-[S.hr, ind] = getHour(S.t, hour/2);
+% 
+% temp = datetime(S.t, 'ConvertFrom','datenum','Format','HH:mm:ss');
+% 
+% [S.hr, ind] = getHour(S.t, hour/2);
 
 [S.s, S.n] = U.xs.xy2sn(S.X, S.Y);
 
 S.zb = n2zb(S.n, U.mesh_mean);
-S.sig = z2sig(S.Z, S.zb, S.eta);
+S.sig = rescale(z2sig(S.Z, S.zb, S.eta), 0, 1); % IS THIS ALLOWED IN SIGMA COORDINATES?? - TAKE CARE
+S.Z = sig2z(S.sig, S.zb, S.eta);
 figure
 plot(S.sig)
 figure;
@@ -104,44 +114,44 @@ scatter3(S.snpN(:,1), S.snpN(:,3), S.snpN(:,4))
 axis equal
 xlabel('t'); ylabel('n'); zlabel('z')
 
-SI = LoessInterpolator();
-SI.known = [S.tsnz_p S.S]';
-SI.span = .1;
-SI.order = 1;
-SI.robust_iterations = 1;
-
-nl = [0, logspace(-2, 2, 29)];
-nnl = length(nl);
-nums = numel(S.S);
-
-for i = 2
-    epsi = randn(size(S.tsnz_p));
-    disp(nl(i))
-    tic
-    SI1(:,i) = SI.interpolate(S.tsnz_p' + nl(i)*epsi');
-    t1 = toc;
-    tic
-    SI2(:,i) = griddatan(S.tsnz_p, S.S, S.tsnz_p + nl(i)*epsi); %150 times slower than loess in standard config
-    t2 = toc;
-    nor(i,1) = i;
-
-    nor(i,2) = sum(abs(S.S - SI1(:,i)).^2, 'omitnan');
-    nor(i,3) = sum(isnan(SI1(:,i)))/nums;
-    nor(i,4) = t1;
-
-    nor(i,5) = sum(abs(S.S - SI2(:,i)).^2, 'omitnan');
-    nor(i,6) = sum(isnan(SI2(:,i)))/nums;
-    nor(i,7) = t2;
-    disp(['LOESS and GDN comparison: ', num2str(nor(i,:))])
-end
-
-
-[SS, ind] = sort(S.S);
-plot(SS)
-hold on
-plot(SI1(ind,i))
-plot(SI2(ind,i))
-legend('Salt', 'SI1', 'SI2')
+% SI = LoessInterpolator();
+% SI.known = [S.snpN S.S]';
+% SI.span = .1;
+% SI.order = 1;
+% SI.robust_iterations = 1;
+% 
+% nl = [0, logspace(-2, 2, 29)];
+% nnl = length(nl);
+% nums = numel(S.S);
+% 
+% for i = 1:10
+%     epsi = randn(size(S.snpN));
+%     disp(nl(i))
+%     tic
+%     SI1(:,i) = SI.interpolate(S.snpN' + nl(i)*epsi');
+%     t1 = toc;
+%     tic
+%     SI2(:,i) = griddatan(S.snpN, S.S, S.snpN + nl(i)*epsi); %150 times slower than loess in standard config
+%     t2 = toc;
+%     nor(i,1) = i;
+% 
+%     nor(i,2) = sum(abs(S.S - SI1(:,i)).^2, 'omitnan');
+%     nor(i,3) = sum(isnan(SI1(:,i)))/nums;
+%     nor(i,4) = t1;
+% 
+%     nor(i,5) = sum(abs(S.S - SI2(:,i)).^2, 'omitnan');
+%     nor(i,6) = sum(isnan(SI2(:,i)))/nums;
+%     nor(i,7) = t2;
+%     disp(['LOESS and GDN comparison: ', num2str(nor(i,:))])
+% end
+% 
+% 
+% [SS, ind] = sort(S.S);
+% plot(SS)
+% hold on
+% plot(SI1(ind,i))
+% plot(SI2(ind,i))
+% legend('Salt', 'SI1', 'SI2')
 
 
 % [x, y] = meshgrid(1:nums, 1:nnl);
